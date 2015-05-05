980d4d2c56a8a8599356443416d995f5808cd9b5
Noma Dube <n.dube@saeny.net>
2015-05-05 16:13:00 -0400

    ok

--- ratchetradio.rb
+++ ratchetradio.rb
@@ -1,14 +1,36 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bundler/setup'
 require 'rack-flash'
 require 'geocoder'
 
 set :database, "sqlite3:example.sqlite3"
 
+get '/layout' do
+	erb :layout
+end	
+
 get '/' do 
-	erb :rockstar
+	erb :home
 end
 
-get '/about' do
-	erb :about
+get '/home' do 
+	erb :home
 end
+
+get '/signup' do
+	erb :signup
+end
+
+post '/signup' do
+	@user = User.new(params[:user])
+	if user.save
+		redirect '/'
+	else
+		redirect '/signup'
+	end
+end	
+
+get '/upload' do
+	erb :upload
+end			
